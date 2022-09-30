3d2063bcd98f1bd11b999062f8b80a2f730e93b3
Shokker <rcheiko@student.42nice.fr>
2022-09-30 20:00:44 +0200

    update: deleted localStorage and check directly to the api if is connected

--- src/components/metamask_button.vue
+++ src/components/metamask_button.vue
@@ -1,11 +1,11 @@
 <template>
     <div v-if="!isLoggedIn && networkMetamask">
-        <button @click="connectWallet()" v-if="isMetamaskSupported || checkIfConnected()">Connect to metamask</button>
+        <button @click="connectWallet()" v-if="isMetamaskSupported">Connect to metamask</button>
         <h2 v-else>Install Metamask Extension</h2>
         <h3 v-if="ready === false">{{errorMessage}}</h3>
     </div>
     <div v-if="isLoggedIn">
-        <p v-if="checkIfConnected()">We are connected : {{account}}</p>
+        <p>We are connected : {{account}}</p>
         <button @click="disconnectWallet()">Disconnect</button>
     </div>
     <div v-if="!networkMetamask">
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from '@vue/runtime-core'
+import {ref} from '@vue/runtime-core'
 import { useTimeout } from '@vueuse/core'
 
 const isMetamaskSupported = ref(false); // Check if the user have metamask and if it's supported
@@ -23,51 +23,38 @@ const isLoggedIn = ref(false); // Check if the user is log
 const account = ref(); // Address of the user
 const errorMessage = ref(); // We are doing error here
 const networkMetamask = ref(false); // Check if the user are on the good network
-const { ready, start } = useTimeout(2500, { controls: true })
+const { ready, start } = useTimeout(2500, { controls: true });
 
-onMounted(async() => {
+(window as any).ethereum.on('connect', async(connectInfo: any) => {
+    await new Promise(resolve => setTimeout(resolve, 0));
     isMetamaskSupported.value = typeof(window as any).ethereum !== 'undefined';
 
-    if (checkIfConnected() === true)
+    if ((window as any).ethereum.selectedAddress) {
         isLoggedIn.value = true;
-
-    // console.log('CONNECTED :', (window as any).ethereum.isConnected());
+        account.value = (window as any).ethereum.selectedAddress;
+    }
 
     const chainId = await (window as any).ethereum.request({ method: 'eth_chainId' });
+
     if (chainId === process.env.VUE_APP_CHAIN_ID) {
         networkMetamask.value = true;
     }
 });
 
-const checkIfConnected = () => {
-    if (localStorage.hasOwnProperty('address')) {
-        account.value = localStorage.address
-        return true;
-    }
-    return false;
-}
-
 (window as any).ethereum.on('accountsChanged', (accounts:any) => {
-    if (localStorage.hasOwnProperty('address')) {
-        const user = accounts[0];
-        if (!user) {
-            isLoggedIn.value = false;
-            errorMessage.value = "You have disconnected, please reconnect."
-            start();
-            localStorage.removeItem('address')
-
-        }
-        else {
-            localStorage.setItem('address', user);
-            account.value = localStorage.address;
-        }
+    const user = accounts[0];
+    if (!user) {
+        isLoggedIn.value = false;
+        errorMessage.value = "You have disconnected, please reconnect."
+        start();
+        account.value = undefined;
+    }
+    else {
+        account.value = (window as any).ethereum.selectedAddress;
     }
 });
 
 (window as any).ethereum.on('chainChanged', (chainId:string) => { // when you change the network
-    if (localStorage.hasOwnProperty('address')) {
-        localStorage.removeItem('address')
-    }
     // Handle the new chain.
     // Correctly handling chain changes can be complicated.
     // We recommend reloading the page unless you have good reason not to.
@@ -77,20 +64,18 @@ const checkIfConnected = () => {
 const connectWallet = async() => { // when the user try to connect
     await (window as any).ethereum
         .request({ method: 'eth_requestAccounts' })
-        .then((response:any) => { // if the user connect
+        .then(() => { // if the user connect
             isLoggedIn.value = true;
-            const account = response[0];
-            localStorage.setItem('address', account);
+            account.value = (window as any).ethereum.selectedAddress;
         })
-        .catch((err:any) => { // if the user cancel the connexion
-            start();
+        .catch(() => { // if the user cancel the connexion
             errorMessage.value = "You have cancelled your connexion."
+            start();
         });
 };
 
 const disconnectWallet = async() => { // when the user disconnect his metamask
     isLoggedIn.value = false;
-    localStorage.removeItem('address');
     account.value = undefined;
 }
 
