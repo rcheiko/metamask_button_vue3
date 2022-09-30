301ffb7c435f2e5e8fd4820781a3581980402aa9
rcheiko <rcheiko@student.42nice.fr>
2022-09-30 16:21:19 +0200

    update some change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/a">Analytics</router-link> |
+    <router-link to="/b">Portfolio</router-link>
   </nav>
   <router-view/>
 </template>
